659091e7a6743d912d323c1684566fd944b73626
Michael Campos <mjcampos@users.noreply.github.com>
2021-03-14 12:30:42 -0700

    Setting up basic color map

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,6 +4,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
+	public enum DrawMode {
+		NoiseMap,
+		ColorMap
+	}
+
+	public DrawMode drawMode;
 	public int mapWidth;
 	public int mapHeight;
 	public float noiseScale;
@@ -18,12 +24,31 @@ public class MapGenerator : MonoBehaviour {
 
 	public bool autoUpdate;
 
+	public TerrainType[] regions;
+
 	public void GenerateMap() {
 		float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
-
 		MapDisplay display = FindObjectOfType<MapDisplay>();
+		Color[] colorMap = new Color[mapWidth * mapHeight];
+
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+				float currentHeight = noiseMap[x, y];
+
+				for (int i = 0; i < regions.Length; i++) {
+					if (currentHeight <= regions[i].height) {
+						colorMap[y * mapWidth + x] = regions[i].color;
+						break;
+					}
+				}
+			}
+		}
 		
-		display.DrawNoiseMap(noiseMap);
+		if (drawMode == DrawMode.NoiseMap) {
+			display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+		} else if (drawMode == DrawMode.ColorMap) {
+			display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+		}
 	}
 
 	private void OnValidate() {
@@ -43,4 +68,11 @@ public class MapGenerator : MonoBehaviour {
 			octaves = 0;
 		}
 	}
+}
+
+[System.Serializable]
+public struct TerrainType {
+	public string name;
+	public float height;
+	public Color color;
 }
